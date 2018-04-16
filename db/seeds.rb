b9c5e96b279eb269b5cbaa732e2dba7ef790d721
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-16 15:41:05 +1000

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jets = ["Jetstar", "Jetmoon", "Jetfire", "Jetice", "Jetwater"]
+origins = ["Paris", "Melbourne", "Tokyo", "Saigon", "New York"]
+description = ["Very spacious", "The Rock's personal jet", "Will Smith's jet", "5 star jet", "Jet with swimming pool"]
+prices = [3000, 5000, 2000, 1000, 2500]
+users = [{first_name: "Harrison", email: "harrison@example.com", password: "password"},
+  { first_name: "Michael", email: "michael@example.com", password: "password"}]
+
+puts "Creating 5 users"
+users.each do |user|
+  User.create!(user)
+end
+
+puts "Creating 5 jets"
+
+jets.each_with_index do |jet, index|
+  Jet.create!(name: jet, description: description[index], has_service: true, price: prices[index], user_id: 1 )
+end
+
+puts "Done!"
