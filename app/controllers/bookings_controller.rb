a2a269628d30660c6cf64e366f01c07afc1e844b
michael vitello <michael.vitello@gmail.com>
2018-04-16 17:34:54 +1000

    Created new, create, edit and update method for Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,50 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :destroy]
+
   def index
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  def edit
   end
 
   def new
   end
 
-  def show
+  def create
+    @booking = Booking.new(booking_params)
+    @jet = Jet.find(params[:jet_id])
+    @booking.jet = @jet
+    @booking.user_id = current_user[:id]
+
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render 'jets/show'
+    end
   end
 
-  def edit
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update!(booking_params)
+    redirect_to bookings_path
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to jet_path(@booking.jet)
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:booked_on, :returned_on, :id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
