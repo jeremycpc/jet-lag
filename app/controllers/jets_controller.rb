41b33642938ed26ba1bdddb8847b22f679982f6e
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-16 17:13:07 +1000

    Added create and new method in jets controller, added a view for the new method as well.

--- app/controllers/jets_controller.rb
+++ app/controllers/jets_controller.rb
@@ -3,11 +3,27 @@ class JetsController < ApplicationController
   end
 
   def new
+    @jet = Jet.new
   end
 
+  def create
+    @jet = Jet.new(jet_params)
+    @jet.user = current_user
+    if @jet.save
+      redirect_to jet_path(@jet)
+    else
+      render :new
+    end
+  end
   def show
+
   end
 
   def edit
+  end
+
+  private
+  def jet_params
+    params.require(:jet).permit(:name, :description, :has_service, :origin, :price)
   end
 end
