c1998b1d6f1e88ae1f3aea35cf4addbb09b730b7
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-17 16:58:49 +1000

    Added booking_price to bookings and calculated price for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,6 +22,7 @@ class BookingsController < ApplicationController
     @jet = Jet.find(params[:jet_id])
     @booking.jet = @jet
     @booking.user_id = current_user[:id]
+    @booking.booking_price = calculate_price(@jet, @booking)
     authorize @booking
     if @booking.save!
       redirect_to bookings_path
@@ -41,6 +42,31 @@ class BookingsController < ApplicationController
     authorize @booking
     @booking.destroy
     redirect_to bookings_path
+  end
+
+  def calculate_price(jet, booking)
+    price = jet.price + ((booking.returned_on - booking.booked_on).floor) * 1000
+    final_price = price + (calculate_distance([jet.airport_origin.lat.to_f,jet.airport_origin.lon.to_f],
+      [jet.airport_destination.lat.to_f,jet.airport_destination.lon.to_f]))* 1.2
+
+    return final_price
+  end
+
+  def calculate_distance(loc1, loc2)
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    return (rm * c)/1000 # Delta in meters
   end
 
 private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417030741) do
+ActiveRecord::Schema.define(version: 20180417061804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180417030741) do
     t.bigint "jet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_price"
     t.index ["jet_id"], name: "index_bookings_on_jet_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
